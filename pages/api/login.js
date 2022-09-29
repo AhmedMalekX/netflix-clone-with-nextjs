ab21de2e909ff27d69ceb5dc5db4fac93a4f6ed7
Ahmed Malek <ahmedmalekvic@mail.ru>
2022-09-29 08:31:02 +0200

    Add rating [like, dislike] functionality to the video page

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,24 @@
-import { magicAdmin } from "../../lib/magic";
-import jwt from "jsonwebtoken";
 import { createNewUser, isNewUser } from "../../lib/db/hasura";
 import { setTokenCookie } from "../../lib/cookies";
+import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
+import { magic } from "../../lib/magic-client";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
-      const didToken = req.headers.authorization.split(" ")[1];
-
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substr(7) : "";
+      await magicAdmin.token.validate(didToken);
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
 
-      const token = await jwt.sign(
+      console.log({ metadata });
+
+      const token = jwt.sign(
         {
           ...metadata,
           iat: Math.floor(Date.now() / 1000),
-          exp: Math.floor((Date.now() / 1000) * 7 * 24 * 60 * 60),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
           "https://hasura.io/jwt/claims": {
             "x-hasura-allowed-roles": ["user", "admin"],
             "x-hasura-default-role": "user",
@@ -25,15 +29,14 @@ export default async function login(req, res) {
       );
 
       const isNewUserQuery = await isNewUser(token, metadata.issuer);
-
       isNewUserQuery && (await createNewUser(token, metadata));
-      await setTokenCookie(token, res);
+      setTokenCookie(token, res);
       res.send({ done: true });
     } catch (error) {
-      console.error("Something went wrong logging in", error.message);
-      res.status(500).json({ done: false });
+      console.error("Something went wrong logging in", error);
+      res.status(500).send({ done: false });
     }
   } else {
-    res.json({ done: false });
+    res.send({ done: false });
   }
 }
--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -4,6 +4,9 @@ import styles from "../../styles/Video.module.css";
 import cls from "classnames";
 import { getYtVideoById } from "../../lib/videos";
 import { Navbar } from "../../components/Navbar/Navbar";
+import Like from "../../components/icons/like-icon";
+import DisLike from "../../components/icons/dislike-icon";
+import { useEffect, useState } from "react";
 
 Modal.setAppElement("#__next");
 
@@ -30,9 +33,66 @@ export async function getStaticPaths() {
 }
 
 const Video = ({ video }) => {
+  const [toggleLike, setToggleLike] = useState(false);
+  const [toggleDisLike, setToggleDisLike] = useState(false);
+
   const router = useRouter();
 
   const { videoId } = router.query;
+
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(`/api/stats?videoId=${videoId}`);
+
+      const data = await response.json();
+
+      if (data.length > 0) {
+        const favourited = data[0].favourited;
+        if (favourited === 1) {
+          setToggleLike(true);
+        } else if (favourited === 0) {
+          setToggleDisLike(true);
+        }
+      }
+    })();
+  }, [videoId]);
+
+  const runRatingServices = async (favourited) => {
+    return await fetch("/api/stats", {
+      method: "POST",
+      body: JSON.stringify({
+        videoId,
+        favourited,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  const handleToggleLike = async () => {
+    const val = !toggleLike;
+    setToggleLike(val);
+    setToggleDisLike(toggleLike);
+
+    const favourited = val ? 1 : 0;
+
+    const response = await runRatingServices(favourited);
+
+    console.log("data", await response.json());
+  };
+  const handleToggleDislike = async () => {
+    const val = !toggleDisLike;
+
+    setToggleDisLike(val);
+    setToggleLike(toggleDisLike);
+
+    const favourited = val ? 0 : 1;
+
+    const response = await runRatingServices(favourited);
+
+    console.log("data", await response.json());
+  };
 
   return (
     <div className={styles.container}>
@@ -54,6 +114,21 @@ const Video = ({ video }) => {
           frameBorder="0"
           className={styles.videoPlayer}
         ></iframe>
+
+        <div className={styles.likeDislikeBtnWrapper}>
+          <button onClick={handleToggleLike}>
+            <div className={styles.likeBtnWrapper}>
+              <div className={styles.btnWrapper}>
+                <Like selected={toggleLike} />
+              </div>
+            </div>
+          </button>
+          <button onClick={handleToggleDislike}>
+            <div className={styles.btnWrapper}>
+              <DisLike selected={toggleDisLike} />
+            </div>
+          </button>
+        </div>
 
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
